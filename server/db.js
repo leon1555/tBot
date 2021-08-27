45009425ead645fafd60d07207ca2fd1d9750623
leon1555 <leon.chisholm@keyin.com>
2021-08-27 12:18:45 -0230

    simplified db function

---
+++ server/db.js
@@ -0,0 +1,21 @@
+const { MongoClient } = require('mongodb');
+const ttrack_password = require('../../ttrack_password/ttrack_password')
+const uri = `mongodb+srv://leon:${ttrack_password}@sprint3.05fm9.mongodb.net/sprint3?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// client.connect(err => {
+//     if(err) throw err
+//     const collection = client.db("sprint3").collection("ttrack")
+//     let username = 'leon'
+//     let query = { username: username}
+//     collection.find(query).toArray(function (err, result) {
+//         if(err) throw err;
+//         if(result.length > 0) {
+//             console.log('Username already exists! Please log in or choose a different username.')
+//         }
+//         client.close();
+//     });
+    
+// });
+
+module.exports = client
