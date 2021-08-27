d7ce8b34e810aa2ef36c239567e009b5790a0316
leon1555 <leon.chisholm@keyin.com>
2021-08-27 12:20:14 -0230

    cupdates to signup post

--- server/index.js
+++ server/index.js
@@ -1,17 +1,8 @@
 const express = require('express')
 const session = require('express-session')
-const path = require('path')
-const pg = require('pg')
 const bcrypt = require('bcrypt')
-const ttrack_password = require('../../ttrack_password/ttrack_password')
-
-const pool = new pg.Pool({
-    user: 'tbot',
-    host: 'localhost',
-    database: 'ttrack',
-    password: ttrack_password,
-    port: 5432
-})
+const client = require('./db')
+const { getSignUp } = require('./controllers')
 
 const app = express()
 
@@ -24,7 +15,7 @@ app.use(
 )
 
 // fetch css
-app.use('/css/', express.static(path.join(__dirname, 'css')))
+// app.use('/css/', express.static(path.join(__dirname, 'css')))
 
 // routes with GET and POST requests
 app.get('/', (req, res) => {
@@ -37,11 +28,51 @@ app.get('/', (req, res) => {
 })
 
 // sign up
-app.get('signup', (req, res) => {
+app.get('/signup', getSignUp)
+
+app.post('/signup', (req, res) => {
     let username = req.body.username
     let password = req.body.password
     let password_confirm = req.body.confirm_password
     if(!username || !password || !password_confirm) {
         res.send('Error! Please fill in all fields')
+    } else {
+        let query = { username: username }
+        const collection = client.db("sprint3").collection("ttrack")
+        client.connect(err => {
+            if(err) throw err
+            collection.find(query).toArray(async function (err, result) {
+                if(err) throw err;
+                if(result.length > 0) {
+                    res.send('Username already exists! Log in or select a different username')
+                } else {
+                    if(password !== password_confirm) {
+                        res.send('Password confirmation does not match password. Please try again.')
+                    } else {
+                        let encryptedPassword = await bcrypt.hash(password, 10)
+                        let newDocument = { username: username, password: encryptedPassword}
+                        collection.insertOne(newDocument, function(err, res) {
+                            if(err) throw err
+                            console.log('New user added!')
+                        })
+                        res.send('New user added!')
+                    }
+                    
+                }
+            });
+        });
     }
-})
+});
+            
+    //     });
+    //     if (password !== password_confirm) {
+    //         res.send('Password confirmation does not match password. Please try again.')
+    //     }
+    //     console.log(password, password_confirm)
+    // } else {
+    //     
+    //     let results = await 
+    // }
+app.listen(3004, function () {
+    console.log("Listening at http://localhost:3004");
+});
