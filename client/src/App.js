5652ce43c5394c597f29706d4836d19b724099cd
leon1555 <leon.chisholm@keyin.com>
2021-08-31 21:34:25 -0230

    added dashboard and profile component

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,22 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
+import { clearCurrentProfile } from "./actions/profileActions";
+
 import { Provider } from "react-redux";
 import store from "./store";
-import { setCurrentUser, logoutUser } from "./actions/authActions";
+
+import PrivateRoute from "./components/common/PrivateRoute";
 
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import Dashboard from "./components/dashboard/Dashboard";
+import CreateProfile from "./components/create-profile/CreateProfile";
 
 import "./App.css";
 
@@ -29,6 +35,7 @@ if (localStorage.jwtToken) {
     // Logout user
     store.dispatch(logoutUser());
     // TODO: Clear current Profile
+    store.dispatch(clearCurrentProfile());
     // Redirect to login
     window.location.href = "/login";
   }
@@ -44,6 +51,16 @@ function App() {
           <div className="container">
             <Route path="/register" exact component={Register} />
             <Route path="/login" exact component={Login} />
+            <Switch>
+              <PrivateRoute path="/dashboard" exact component={Dashboard} />
+            </Switch>
+            <Switch>
+              <PrivateRoute
+                path="/create-profile"
+                exact
+                component={CreateProfile}
+              />
+            </Switch>
           </div>
           <Footer />
         </div>
