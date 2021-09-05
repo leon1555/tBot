103ad0ab7dc4b2cea8d163114d14b8241178c8de
leon1555 <leon.chisholm@keyin.com>
2021-09-04 21:33:38 -0230

    tweaks to logo, font

--- client/src/components/dashboard/ProfileActions.js
+++ client/src/components/dashboard/ProfileActions.js
@@ -5,15 +5,11 @@ const ProfileActions = () => {
   return (
     <div className="btn-group mb-4" role="group">
       <Link to="/edit-profile" className="btn btn-light">
-        <i className="fas fa-user-circle text-info mr-1"></i> Edit Profile
+        <i className="fas fa-user-ninja mr-1"></i> Edit Profile
       </Link>
       <Link to="add-injection" className="btn btn-light">
-        <i className="fab fa-black-tie text-info mr-1"></i>
+        <i className="fas fa-syringe mr-1"></i>
         Add Injection
-      </Link>
-      <Link to="add-education" className="btn btn-light">
-        <i className="fas fa-graduation-cap text-info mr-1"></i>
-        Add Education
       </Link>
     </div>
   );
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -61,7 +61,7 @@ class Navbar extends Component {
     return (
       <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
         <div className="container">
-          <Link className="navbar-brand risque-font" to="/">
+          <Link className="navbar-brand risque-font-light" to="/">
             <img
               src={logo}
               alt=""
